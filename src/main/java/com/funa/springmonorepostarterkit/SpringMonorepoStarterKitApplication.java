e089129bf70069dd91a71952745d087ac284c77c
funa <photiary@gmail.com>
2025-07-08 19:48:12 +0900

    init

---
+++ src/main/java/com/funa/springmonorepostarterkit/SpringMonorepoStarterKitApplication.java
@@ -0,0 +1,13 @@
+package com.funa.springmonorepostarterkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonorepoStarterKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMonorepoStarterKitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/funa/springmonorepostarterkit/SpringMonorepoStarterKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funa.springmonorepostarterkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonorepoStarterKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
